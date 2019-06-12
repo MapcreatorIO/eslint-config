0d2e4c69dead49e0210584c207ae28cd033f4d0f
Kevin Richter <k.richter1994@gmail.com>
2019-06-12 13:50:59 +0200

    Fix no-extra-parens rule, v1.0.3

--- rules/es6/possibleErrors.js
+++ rules/es6/possibleErrors.js
@@ -5,7 +5,7 @@ module.exports = {
     'no-async-promise-executor': 'error',
     'no-console': prod('off', 'warn'),
     'no-debugger': prod('off'),
-    'no-extra-parens': prod(),
+    'no-extra-parens': [prod(), 'all', { nestedBinaryExpressions: false, enforceForArrowConditionals: false }],
     'no-misleading-character-class': 'error',
     'no-template-curly-in-string': 'error',
   },
