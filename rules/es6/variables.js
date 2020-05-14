f8de9915b79d77540f7c3eca3206403ca12737f8
Kevin Richter <k.richter1994@gmail.com>
2020-05-14 10:13:33 +0200

    v1.1.3 allow vars starting with _

--- rules/es6/variables.js
+++ rules/es6/variables.js
@@ -5,7 +5,7 @@ module.exports = {
     'no-shadow': prod(),
     'no-undef': 'error',
     'no-undef-init': 'error',
-    'no-unused-vars': prod(),
+    'no-unused-vars': [prod(), { 'vars': 'all', 'args': 'none', varsIgnorePattern: '^_' }],
     'no-use-before-define': ['error', { functions: false }],
   },
 };
